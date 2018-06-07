8a8d1732e6bda6c6fdc304e0c1f36db8b2ff7ba7
dfabianladino <dfabianladino@gmail.com>
2018-06-07 11:56:35 -0500

    Proyecto Base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//Import Material Module
+import { MaterialModule } from './material.module';
+import 'hammerjs';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
